5c30f516b7c2d7db8f154caeea873a072c76e8b7
David Magar <ddm2084@utulsa.edu>
2018-01-30 01:10:48 -0600

    Hash function fixes. Still facing issues with file input.

--- GenomeSearch/P1.cpp
+++ GenomeSearch/P1.cpp
@@ -1,3 +1,4 @@
+//#include "myfunctions.h"
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
@@ -6,6 +7,7 @@
 
 using namespace std;
 
+int hashWord(const std::string& buffer);
 
 int main(int argc, char* argv[]) {
 
@@ -14,12 +16,11 @@ int main(int argc, char* argv[]) {
 	ifstream inputFile(argv[2]);
 	char data;
 	string buffer;
+	inputFile.get(data);
 
-	
-		
 	while(!inputFile.eof()){
-		while(counter < length){	
-			inputFile.get(data);
+		while(counter < length){
+			inputFile.get(data);				
 			if(isalpha(data)){
 				buffer.push_back(data);
 				counter++;
@@ -30,28 +31,60 @@ int main(int argc, char* argv[]) {
 			
 		}
 		cout << buffer << endl;
+		
+		//push to functions
+		hashWord(buffer);
+
+
+		//Erase first character of string
 		buffer.erase(0,1);
-		counter = counter-1;	
+		inputFile.get(data);
+		//Decriment counter so new character can be pulled i	
+		counter = counter - 1;	
+		
+
+	}
+	cout << "got here too" << endl;
+	inputFile.close();
+	return 0;
+}
+
+int hashWord(const std::string& buffer){
+	int bufLength = buffer.length();
+	int total;
+	int addend;
+	for(int i = 0; i < bufLength; i++){
+		if(buffer.at(i) == 'a' || buffer.at(i) == 'A'){
+			std::cout << 1;
+			addend = 1;
+		}
+		else if(buffer.at(i) == 'c' || buffer.at(i) == 'C'){
+			std::cout << 2;
+			addend = 2;
+		}
+		else if(buffer.at(i) == 'g' || buffer.at(i) == 'G'){
+			std::cout << 3;
+			addend = 3;
+		}
+		else if(buffer.at(i) == 't' || buffer.at(i) == 'T'){
+			std::cout << 4;
+			addend = 4;
+		}
+		else{
+			std::cout << endl;
+			//return -1;
+		}
+		total << 2;
+		total = total|addend;
+		cout << total << endl;
 	}
 	
-		
-	cout << buffer << endl;
-
-//	for (vector<char>::const_iterator i = name.begin(); i != 		name.end(); ++i)
-//    std::cout << *i;
-
-//	inputFile.close();
-
 	return 0;
 }
 
 
 
 
-
-int hashWord(string){
-
-}
 
 int updateWord(string){
 
@@ -64,6 +97,4 @@ vector<int> updateConsecutiveWords(string, int){
 void printTable(){
 
 }
-//convertToHash(){
 
-//}
