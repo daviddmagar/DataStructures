868efeb62a142320ea6f9d3dd3cca689d507d4b7
David Magar <ddm2084@utulsa.edu>
2018-01-30 01:54:02 -0600

    Fixed eof file error with different implentation. Also updated method of getting chars from file

--- GenomeSearch/P1.cpp
+++ GenomeSearch/P1.cpp
@@ -11,75 +11,93 @@ int hashWord(const std::string& buffer);
 
 int main(int argc, char* argv[]) {
 
+	//Set word length to command line argument
 	int length = atoi(argv[1]);
-	int counter = 0;
+	
+	//Get filepath of genome from command line
 	ifstream inputFile(argv[2]);
+	
+	int counter = 0;	
 	char data;
 	string buffer;
-	inputFile.get(data);
 
-	while(!inputFile.eof()){
-		while(counter < length){
-			inputFile.get(data);				
+
+	//While not end of file
+	while(inputFile >> data){
+			//if input is letter add to buffer
 			if(isalpha(data)){
 				buffer.push_back(data);
 				counter++;
 			}
-			else{
-				continue;
+			//Else skip it
+			else continue;
+		while(counter < length){
+			inputFile >> data;	
+			//If input is letter add to buffer			
+			if(isalpha(data)){
+				buffer.push_back(data);
+				counter++;
 			}
-			
+			//Else skip it
+			else continue;
 		}
-		cout << buffer << endl;
 		
 		//push to functions
 		hashWord(buffer);
 
 
+
 		//Erase first character of string
 		buffer.erase(0,1);
-		inputFile.get(data);
 		//Decriment counter so new character can be pulled i	
-		counter = counter - 1;	
-		
+		counter = counter - 1;
 
 	}
-	cout << "got here too" << endl;
 	inputFile.close();
 	return 0;
 }
 
+//Get string from file
 int hashWord(const std::string& buffer){
+	cout << buffer << endl;
 	int bufLength = buffer.length();
 	int total;
 	int addend;
+	//iterate through buffer
 	for(int i = 0; i < bufLength; i++){
+		//convert a to 1
 		if(buffer.at(i) == 'a' || buffer.at(i) == 'A'){
 			std::cout << 1;
 			addend = 1;
 		}
+		//convert c to 2
 		else if(buffer.at(i) == 'c' || buffer.at(i) == 'C'){
 			std::cout << 2;
 			addend = 2;
 		}
+		//convert g to 3
 		else if(buffer.at(i) == 'g' || buffer.at(i) == 'G'){
 			std::cout << 3;
 			addend = 3;
 		}
+		//convert t to 4
 		else if(buffer.at(i) == 't' || buffer.at(i) == 'T'){
 			std::cout << 4;
 			addend = 4;
 		}
+		//If illegal character return -1 and skip rest of calculations
 		else{
 			std::cout << endl;
-			//return -1;
+			return -1;
 		}
+		//binary shift total twice 
 		total << 2;
+		//or total with the addend
 		total = total|addend;
 		cout << total << endl;
 	}
-	
-	return 0;
+	//return the total
+	return total;
 }
 
 
