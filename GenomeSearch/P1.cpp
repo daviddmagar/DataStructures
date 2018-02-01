597d7a3a93aebc4607c61a6e9fd2b7cc7b1bf218
David Magar <ddm2084@utulsa.edu>
2018-02-01 02:00:20 -0600

    Fixed hashing function. Writes to vector, prints.

--- GenomeSearch/P1.cpp
+++ GenomeSearch/P1.cpp
@@ -7,44 +7,87 @@
 
 using namespace std;
 
-int hashWord(const std::string& buffer);
+int hashWord(std::string);
+int updateWord(std::string);
+vector<int> updateConsecutiveWords(std::string, int);
+void printTable();
+vector<int> hashConsecutiveWords(string, int);
+vector<int> updateConsecutiveWords(string, int);
+
+
+//Globals
+int length;
+ifstream inputFile;
+std::vector<int> storageTable;
 
 int main(int argc, char* argv[]) {
-
+	
+	
 	//Set word length to command line argument
-	int length = atoi(argv[1]);
+	length = atoi(argv[1]);
+	int maxLength = 4;
+	
+	for(int i = length; i > 1; i--){
+		maxLength = maxLength*4;
+		}
+		
+
+	
+	storageTable.resize(maxLength);
+	
+	
+	
+	
+	
+	
 	
 	//Get filepath of genome from command line
-	ifstream inputFile(argv[2]);
+	inputFile.open(argv[2]);
 	
 	int counter = 0;	
 	char data;
+	int returnedHash;
 	string buffer;
 
 
 	//While not end of file
-	while(inputFile >> data){
+	while(!inputFile.eof()){
 			//if input is letter add to buffer
-			if(isalpha(data)){
+			inputFile.get(data);
+			if(data == '\n'){
+				continue;
+			}
+				
 				buffer.push_back(data);
 				counter++;
+			
+			
+		while(buffer.size() < length){
+			inputFile.get(data);
+			if(data == '\n'){
+				continue;
 			}
-			//Else skip it
-			else continue;
-		while(counter < length){
-			inputFile >> data;	
+			if(inputFile.eof()){
+				break;	
+			}
 			//If input is letter add to buffer			
-			if(isalpha(data)){
-				buffer.push_back(data);
-				counter++;
-			}
-			//Else skip it
-			else continue;
+			buffer.push_back(data);
+			counter++;
+
 		}
 		
 		//push to functions
-		hashWord(buffer);
-
+		returnedHash = updateWord(buffer);
+		if(!(returnedHash == -1)){
+			storageTable.at(returnedHash) = storageTable.at(returnedHash) + 1;
+			vector<int> returnedHashes = updateConsecutiveWords(buffer, returnedHash);
+			cout << returnedHashes.size() << endl;
+			for(int i = 0; i < returnedHashes.size(); i++){
+				storageTable.at(returnedHashes.at(i)) = storageTable.at(returnedHashes.at(i)) + 1;
+			}
+			buffer.erase(0, length);
+			cout << buffer << endl;
+		}
 
 
 		//Erase first character of string
@@ -53,48 +96,45 @@ int main(int argc, char* argv[]) {
 		counter = counter - 1;
 
 	}
+	
+	printTable();
 	inputFile.close();
 	return 0;
 }
 
+
+
 //Get string from file
-int hashWord(const std::string& buffer){
-	cout << buffer << endl;
+int hashWord(std::string buffer){
 	int bufLength = buffer.length();
-	int total;
-	int addend;
+	int total = 0;
+	int addend = 0;
 	//iterate through buffer
 	for(int i = 0; i < bufLength; i++){
 		//convert a to 1
 		if(buffer.at(i) == 'a' || buffer.at(i) == 'A'){
-			std::cout << 1;
-			addend = 1;
+			addend = 0;
 		}
 		//convert c to 2
 		else if(buffer.at(i) == 'c' || buffer.at(i) == 'C'){
-			std::cout << 2;
-			addend = 2;
+			addend = 1;
 		}
 		//convert g to 3
 		else if(buffer.at(i) == 'g' || buffer.at(i) == 'G'){
-			std::cout << 3;
-			addend = 3;
+			addend = 2;
 		}
 		//convert t to 4
 		else if(buffer.at(i) == 't' || buffer.at(i) == 'T'){
-			std::cout << 4;
-			addend = 4;
+			addend = 3;
 		}
 		//If illegal character return -1 and skip rest of calculations
 		else{
-			std::cout << endl;
 			return -1;
 		}
 		//binary shift total twice 
-		total << 2;
+		total = total << 2;
 		//or total with the addend
 		total = total|addend;
-		cout << total << endl;
 	}
 	//return the total
 	return total;
@@ -104,15 +144,107 @@ int hashWord(const std::string& buffer){
 
 
 
-int updateWord(string){
-
+int updateWord(std::string buffer){
+	int hashbuffer = hashWord(buffer);
+	return hashbuffer;
 }
 
-vector<int> updateConsecutiveWords(string, int){
-
+vector<int> updateConsecutiveWords(string buffer, int hashBuffer){
+	vector<int> namesTable = hashConsecutiveWords(buffer, hashBuffer);
+	return namesTable;
 }
+
+vector<int> hashConsecutiveWords(string name, int passedHash){
+	vector<int> namesTable;
+	char temp;
+	int tempNum;
+
+	while(!inputFile.eof()){
+			inputFile.get(temp);
+	
+			if(temp == '\n'  || inputFile.eof()){
+				continue;
+			}
+			temp = toupper(temp);
+			
+			//if input is letter add to buffer
+			if(temp == 'A'){
+				tempNum = 0;
+			}
+			else if(temp == 'C'){
+				tempNum = 1;
+			}
+			else if(temp == 'G'){
+				tempNum = 2;
+			}
+			else if(temp == 'T'){
+				tempNum = 3;
+			}
+			else{
+
+				return namesTable;
+			}
+			
+			
+			name.erase(0,1);
+			name = name + temp;	
+			
+			//minimum mask for word length two, 15 = 1111	
+			int mask = 15; 
+			
+			//minimal mask for word length as per specs
+			if(length > 2){
+				int updateMask = length-2;
+				for(int i = 0; i < updateMask; i++){
+					//shift for 
+					mask = mask << 2; //shift needed for length of name
+					mask = mask | 3; //3 = 0011
+					}
+				}
+			
+			passedHash = passedHash << 2; //scoot over for next letter
+			passedHash = passedHash | tempNum; //binary add of new char
+			passedHash = passedHash & mask; //get rid of leading numbers
+			namesTable.push_back(passedHash);		
+	}
+					cout << namesTable.size() << endl;
+	return namesTable;
+
+	
+	
+}
+	
 
 void printTable(){
-
-}
+	
+	string word = "";
+	int mask = 3;//binary 0011
+	int tempStore = 0;
+	int hash = 0;
+	
+	for(int i =0; i < storageTable.size(); i++){
+		if(storageTable.at(i) != 0){
+			hash = i;
+			for(int x = 0; x < length; x++){
+				tempStore = hash & mask;
+				if(tempStore == 0)
+					word.insert(0, "A");
+				else if(tempStore == 1)
+					word.insert(0, "C");
+				else if(tempStore == 2)
+					word.insert(0, "G");
+				else if(tempStore == 3)
+					word.insert(0, "T");
+				hash = hash >> 2;
+				
+				tempStore = 0;
+				}
+				
+				
+				cout << word << " " << storageTable.at(i) << endl;
+				word = "";
+			}
+		}
+			
+	}
 
--- GenomeSearch/README.md
+++ GenomeSearch/README.md
@@ -1,3 +1,3 @@
 Program to search for all unique words of a given lentgh in a genome 
 sequence. Length of word and file path of genome are passed as command 
-line arguments. Code is written in C++
+line arguments. Code is written in C++.
